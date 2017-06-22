a92df79e396970abc1057de46b067e0305b62550
Tero Marttila <tero.marttila@kontena.io>
2017-06-22 15:45:16 +0300

    spec server node plugger/unplugger status

--- server/spec/services/agent/node_plugger_spec.rb
+++ server/spec/services/agent/node_plugger_spec.rb
@@ -8,17 +8,20 @@ describe Agent::NodePlugger do
     allow(subject).to receive(:rpc_client).and_return(rpc_client)
   end
 
-  context 'for a brand new node' do
+  context 'for an initializing node' do
     let(:node) {
-      HostNode.create!(node_id: 'xyz')
+      HostNode.create!(grid: grid, node_id: 'xyz')
     }
 
     it 'marks node as connected' do
       expect(subject).to receive(:send_master_info)
       expect(subject).to receive(:send_node_info)
+
+      expect(node.status).to eq :connecting
       expect {
         subject.plugin! connected_at
       }.to change{ node.reload.connected? }.to be_truthy
+      expect(node.status).to eq :connecting
     end
   end
 
@@ -27,7 +30,7 @@ describe Agent::NodePlugger do
       HostNode.create!(
         node_id: 'xyz',
         grid: grid, name: 'test-node', labels: ['region=ams2'],
-        connected: false,
+        connected: false, updated: true,
         private_ip: '10.12.1.2', public_ip: '80.240.128.3',
       )
     }
@@ -40,6 +43,7 @@ describe Agent::NodePlugger do
         expect {
           subject.plugin! connected_at
         }.to change{ node.reload.connected? }.to be_truthy
+        expect(node.status).to eq :connecting
       end
     end
 
@@ -71,7 +75,7 @@ describe Agent::NodePlugger do
       HostNode.create!(
         node_id: 'xyz',
         grid: grid, name: 'test-node', labels: ['region=ams2'],
-        connected: true, connected_at: reconnected_at,
+        connected: true, connected_at: reconnected_at, updated: true,
         private_ip: '10.12.1.2', public_ip: '80.240.128.3',
       )
     }
@@ -81,7 +85,7 @@ describe Agent::NodePlugger do
         expect(subject).to_not receive(:publish_update_event)
         expect(subject).to_not receive(:send_master_info)
         expect(subject).to_not receive(:send_node_info)
-        
+
         expect {
           subject.plugin! connected_at
         }.to_not change{ node.reload.connected_at }
